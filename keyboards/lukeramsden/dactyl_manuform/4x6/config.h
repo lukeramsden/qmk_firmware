c167f5592e4b7a41a3a58c39b4fb656f602d6d76
lukeramsden <lukeramsden@pm.me>
2020-09-22 21:02:56 +0100

    [lukeramsden/dactyl_manuform/4x6] Updated right-hand pinout

--- keyboards/lukeramsden/dactyl_manuform/4x6/config.h
+++ keyboards/lukeramsden/dactyl_manuform/4x6/config.h
@@ -34,6 +34,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // row-driven
 #define MATRIX_ROW_PINS { F0, F1, C7, D5, B7 }
 #define MATRIX_COL_PINS { B4, B5, E6, D7, C6, D4 }
+#define MATRIX_ROW_PINS_RIGHT { E6, D7, C6, D4, D0, D1 }
+#define MATRIX_COL_PINS_RIGHT { B4, B5, B6, B2, B3 }
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION ROW2COL
